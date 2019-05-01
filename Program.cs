28bdbb0eb5cf2e632eae6a1b023eae4939d91f84
MedCYLue <medlu@MedCYLuede-MacBook-Pro.local>
2019-05-01 22:10:02 +0800

    finish file read and write

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        public static void writeFile()
+        {
+            using(StreamWriter fileWriter = new StreamWriter(new FileStream("toolDAT.DAT", FileMode.Create)))
+            {
+                fileWriter.WriteLine("130");
+            }
+        }
+
+        public static string readFile()
+        {
+            try
+            {
+                using(StreamReader fileReader = new StreamReader("toolDAT.DAT"))
+                {
+                    string line = fileReader.ReadLine().ToString();
+                    if(line != null) return line;
+                    else return "";
+                }
+            }
+            catch { return "Exception";}
+        }
+
+        public static void Main()
+        {
+            writeFile();
+            Console.WriteLine(readFile());
+            Console.ReadLine();
+        }
+    }
+}
